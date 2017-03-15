94401ae4c0c72448405c6d8dacc3bee69de0e52b
Matthew Rice <mtr.rice@gmail.com>
2017-03-14 21:36:47 -0400

    Made header mobile responsive through the use of media queries.

--- app/scripts/components/header.jsx
+++ app/scripts/components/header.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+require('bootstrap-sass');
 
 
 var HeaderComponent = React.createClass({
@@ -13,6 +14,14 @@ var HeaderComponent = React.createClass({
           <a href="#homepage">Home</a>
           <a className="aboutUsNavLink" href="#aboutus">About Us</a>
           <a className="ourShopNavLink" href="#ourshop">Our Shop</a>
+        </div>
+        <div className="mediaQuery650vwNavBarDropdown">
+          <i className="fa fa-bars dropdown-toggle fontawesome-navbars" aria-hidden="true" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></i>
+          <ul className="dropdown-menu">
+            <li className="menu"><a className="navLinks" href="#homepage">Home</a></li>
+            <li className="menu"><a className="navLinks" href="#aboutus">About Us</a></li>
+            <li className="menu"><a className="navLinks" href="#ourshop">Our Shop</a></li>
+          </ul>
         </div>
       </div>
     );
