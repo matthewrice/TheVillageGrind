f17333021ba8149cb62ca615cfd6165fcb95a216
Matthew Rice <mtr.rice@gmail.com>
2017-07-10 12:29:14 -0400

    Completed adding media queries to make the website completely mobile responsive.

--- app/scripts/components/ourShop.jsx
+++ app/scripts/components/ourShop.jsx
@@ -10,15 +10,15 @@ var OurShopComponent = React.createClass({
       <div>
         <HeaderComponent />
         <div className="row">
-          <img className="col-xs-offset-1 col-xs-5 outsideOfShopImage" src="images/outside-of-shop.jpg" />
-          <img className="col-xs-5 extendedMenuImage" src="images/extended-menu.jpg" />
-          <img className="col-xs-offset-1 col-xs-10 backCounterImage" src="images/back-counter.jpg" />
-          <img className="col-xs-offset-1 col-xs-5 closeupOfEspressoMachingImage" src="images/closeup-of-espresso-machine.jpg" />
-          <img className="col-xs-5 pouringLatteImage" src="images/pouring-latte.jpg" />
-          <img className="col-xs-offset-1 col-xs-10 latteImage" src="images/latte.jpg" />
-          <img className="col-xs-offset-1 col-xs-5 mapOnWallImage" src="images/map-on-wall.jpg" />
-          <img className="col-xs-5 menuImage" src="images/menu.jpg" />
-          <img className="col-xs-offset-1 col-xs-10 menuBoardsImage" src="images/menu-boards.jpg" />
+          <img className="col-xs-6 col-md-offset-1 col-md-5 outsideOfShopImage" src="images/outside-of-shop.jpg" />
+          <img className="col-xs-6 col-md-5 extendedMenuImage" src="images/extended-menu.jpg" />
+          <img className="col-xs-12 col-md-offset-1 col-md-10 backCounterImage" src="images/back-counter.jpg" />
+          <img className="col-xs-6 col-md-offset-1 col-md-5 closeupOfEspressoMachingImage" src="images/closeup-of-espresso-machine.jpg" />
+          <img className="col-xs-6 col-md-5 pouringLatteImage" src="images/pouring-latte.jpg" />
+          <img className="col-xs-12 col-md-offset-1 col-md-10 latteImage" src="images/latte.jpg" />
+          <img className="col-xs-6 col-md-offset-1 col-md-5 mapOnWallImage" src="images/map-on-wall.jpg" />
+          <img className="col-xs-6 col-md-5 menuImage" src="images/menu.jpg" />
+          <img className="col-xs-12 col-md-offset-1 col-md-10 menuBoardsImage" src="images/menu-boards.jpg" />
         </div>
         <FooterComponent />
       </div>
