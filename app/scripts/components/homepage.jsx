d7d3e218ec9a658a5432c6f93afa2e435c6933b7
Matthew Rice <mtr.rice@gmail.com>
2017-08-14 16:24:06 -0400

    npm installed google analytics and followed instructions on https://golightlyplus.com/setup-google-analytics-react-js-apps/. Hopefully the google analytics works now.

--- app/scripts/components/homepage.jsx
+++ app/scripts/components/homepage.jsx
@@ -1,9 +1,15 @@
 var React = require('react');
+var ReactGA = require('react-ga');
 
 var HeaderComponent = require('./header.jsx');
 var FooterComponent = require('./footer.jsx');
 
 var HomepageComponent = React.createClass({
+  constructor(){
+    ReactGA.initialize('UA-104489668-1');
+    // This just needs to be called once since we have no routes in this case.
+    ReactGA.pageview(window.location.pathname);
+  };
   render: function(){
     return (
       <div>
